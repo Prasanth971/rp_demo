dcef72c06594bb0e34f2a230e5c12e6ae9473614
Prasanth971 <prasanthseenuvasan.gmail.com>
2026-07-24 17:25:01 +0100

    tpl_chnage

--- contact_flow.tf
+++ contact_flow.tf
@@ -1,10 +1,30 @@
+
+ocals {
+
+lambda_arns = {
+
+dev = "arn:aws:connect:us-west-2:367115304840:instance/dba85ea8-af3e-40d8-ae9c-59302bcd7a23/contact-flow/39be5f6c-fe74-4be9-8624-2484304fe1e5"
+
+
+}
+
+}
+
 resource "aws_connect_contact_flow" "Terraform_Contact_Flow" {
   instance_id  = var.instance_id
   name         = "Terraform_Contact_Flow"
   description  = "Test Contact Flow Description"
   type         = "CONTACT_FLOW"
   filename     = "flow01.json"
-  content_hash = filebase64sha256("flow01.json")
+  content_hash = filebase64sha256("flow01.json"
+  
+  {
+
+lambda_arn = local.lambda_arns[dev]
+
+}
+  
+  )
   tags = {
     "Name"        = "Terraform Contact Flow",
     "Application" = "Terraform",
