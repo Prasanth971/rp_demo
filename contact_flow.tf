5be6a20afc747e6dd06afec013b3a774efad9cce
Prasanth971 <prasanthseenuvasan.gmail.com>
2026-07-24 17:42:56 +0100

    'contactflowchangestpl'

--- contact_flow.tf
+++ contact_flow.tf
@@ -15,14 +15,16 @@ resource "aws_connect_contact_flow" "Terraform_Contact_Flow" {
   description  = "Test Contact Flow Description"
   type         = "CONTACT_FLOW"
   filename     = "flow01.json"
-  content_hash = filebase64sha256("flow01.json"
+  content_hash = base64sha256(templatefile("${path.module}/flow01.json.tpl"
   
   {
 
-lambda_arn = local.lambda_arns[dev]
+lambda_arn = local.lambda_arns["dev"]
 
 }
   
+  )
+
   )
   tags = {
     "Name"        = "Terraform Contact Flow",
