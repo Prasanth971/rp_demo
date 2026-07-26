71eacdf330eb93dd5d2f150061f84f017a78197f
Prasanth971 <prasanthseenuvasan.gmail.com>
2026-07-26 23:06:52 +0100

    's3bucketchanges'

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "tf_backend_01"
+    bucket = "ejtfbackend01"
     key    = "aws.tf"
     region = "us-west-2"
     use_lockfile  = "true"
--- contact_flow.tf
+++ contact_flow.tf
@@ -14,8 +14,6 @@ resource "aws_connect_contact_flow" "Terraform_Contact_Flow" {
   name         = "Terraform_Contact_Flow"
   description  = "Test Contact Flow Description"
   type         = "CONTACT_FLOW"
-  filename     = "flow01.json"
-  content = file("${path.module}/flow01.json.tpl")
   content = templatefile("${path.module}/flow01.json.tftpl", {
     lambda_arn = "arn:aws:connect:us-west-2:367115304840:instance/dba85ea8-af3e-40d8-ae9c-59302bcd7a23/contact-flow/39be5f6c-fe74-4be9-8624-2484304fe1e5"
   })
