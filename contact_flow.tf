ad567fbcfde342b3db03582a896762969efd8b7d
Prasanth971 <prasanthseenuvasan.gmail.com>
2026-07-26 23:49:28 +0100

    samplelambdacode

--- contact_flow.tf
+++ contact_flow.tf
@@ -15,7 +15,7 @@ resource "aws_connect_contact_flow" "Terraform_Contact_Flow" {
   description  = "Test Contact Flow Description"
   type         = "CONTACT_FLOW"
   content = templatefile("${path.module}/flow01.json.tftpl", {
-    lambda_arn = "arn:aws:connect:us-west-2:367115304840:instance/dba85ea8-af3e-40d8-ae9c-59302bcd7a23/contact-flow/39be5f6c-fe74-4be9-8624-2484304fe1e5"
+    lambda_arn = "arn:aws:connect:us-west-2:367115304840:instance/dba85ea8-af3e-40d8-ae9c-59302bcd7a23/contact-flow/88884c2b-f706-4301-8880-ee54704a803b"
   })
 tags = {
     "Name"        = "Terraform Contact Flow",
